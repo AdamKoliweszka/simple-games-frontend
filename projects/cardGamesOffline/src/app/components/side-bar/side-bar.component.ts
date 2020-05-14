315bf029045122604aa4767e98412251654c4472
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-05-15 01:03:43 +0200

    add new style menu

--- projects/cardGamesOffline/src/app/components/side-bar/side-bar.component.ts
+++ projects/cardGamesOffline/src/app/components/side-bar/side-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../../modules/user-of-service/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-side-bar",
@@ -7,11 +8,50 @@ import { AuthService } from "../../modules/user-of-service/services/auth.service
   styleUrls: ["./side-bar.component.scss"],
 })
 export class SideBarComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  private houverItem: string = null;
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
+  get isGamesActive(): boolean {
+    return this.router.url === "/games" || this.houverItem === "games";
+  }
+
+  get isHomeActive(): boolean {
+    return this.router.url === "/home" || this.houverItem === "home";
+  }
+
+  get isChatActive(): boolean {
+    return this.router.url === "/chat" || this.houverItem === "chat";
+  }
+
+  get isFriendsActive(): boolean {
+    return this.router.url === "/friends" || this.houverItem === "friends";
+  }
+
+  get isAchievementsActive(): boolean {
+    return (
+      this.router.url === "/achievements" || this.houverItem === "achievements"
+    );
+  }
+
+  get isSettingsActive(): boolean {
+    return this.router.url === "/settings" || this.houverItem === "settings";
+  }
+
+  get isLogoutActive(): boolean {
+    return this.router.url === "/logout" || this.houverItem === "logout";
+  }
+
   onLogout() {
     this.authService.logoutUser();
+  }
+
+  mouseEnter(item: string) {
+    this.houverItem = item;
+  }
+
+  mouseLeave() {
+    this.houverItem = null;
   }
 }
--- projects/cardGamesOffline/src/app/components/side-bar/side-bar.module.ts
+++ projects/cardGamesOffline/src/app/components/side-bar/side-bar.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MaterialModule } from "../../modules/material/material.module";
 import { SideBarComponent } from "./side-bar.component";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   declarations: [SideBarComponent],
-  imports: [CommonModule, MaterialModule],
-  exports: [SideBarComponent]
+  imports: [CommonModule, MaterialModule, TranslateModule],
+  exports: [SideBarComponent],
 })
 export class SideBarModule {}
