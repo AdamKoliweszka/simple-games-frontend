0432d0a5715e407f2d79f5944d2019a26d814f1f
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-05-25 17:17:32 +0200

    add autoscroll chat

--- projects/cardGamesOffline/src/app/modules/chat/chat-output/chat-output.component.ts
+++ projects/cardGamesOffline/src/app/modules/chat/chat-output/chat-output.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewChecked,
+} from "@angular/core";
 import { ChatMessage } from "../models/chat-message";
 
 @Component({
@@ -6,9 +13,22 @@ import { ChatMessage } from "../models/chat-message";
   templateUrl: "./chat-output.component.html",
   styleUrls: ["./chat-output.component.scss"],
 })
-export class ChatOutputComponent implements OnInit {
+export class ChatOutputComponent implements OnInit, AfterViewChecked {
+  @ViewChild("scrollContainer", { static: false })
+  private scrollContainer: ElementRef;
+
   @Input() chatMessages: ChatMessage[];
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+    } catch (err) {}
+  }
 }
