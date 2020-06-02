f44d4f92e3c5bb0ec410dfaadc6f6d2c4abc3c68
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-06-02 18:39:16 +0200

    improve routing in register and login page

--- projects/cardGamesOffline/src/app/modules/user-of-service/user-of-service-routing.module.ts
+++ projects/cardGamesOffline/src/app/modules/user-of-service/user-of-service-routing.module.ts
@@ -1,26 +1,47 @@
 import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
+import { UserPageComponent } from "./user-page/user-page.component";
 
 const routes: Routes = [
-  {
-    path: "login",
-    loadChildren: () =>
-      import("./user-login-page/user-login-page.module").then(
-        (m) => m.UserLoginPageModule
-      ),
-  },
-  {
-    path: "register",
-    loadChildren: () =>
-      import("./user-register-page/user-register-page.module").then(
-        (m) => m.UserRegisterPageModule
-      ),
-  },
   {
     path: "",
     redirectTo: "login",
     pathMatch: "full",
   },
+  {
+    path: "",
+    component: UserPageComponent,
+    children: [
+      {
+        path: "register",
+        loadChildren: () =>
+          import("./user-register-page/user-register-page.module").then(
+            (m) => m.UserRegisterPageModule
+          ),
+      },
+      {
+        path: "login",
+        loadChildren: () =>
+          import("./user-login-page/user-login-page.module").then(
+            (m) => m.UserLoginPageModule
+          ),
+      },
+    ],
+  },
+  // {
+  //   path: "login",
+  //   loadChildren: () =>
+  //     import("./user-login-page/user-login-page.module").then(
+  //       (m) => m.UserLoginPageModule
+  //     ),
+  // },
+  // {
+  //   path: "register",
+  //   loadChildren: () =>
+  //     import("./user-register-page/user-register-page.module").then(
+  //       (m) => m.UserRegisterPageModule
+  //     ),
+  // },
 ];
 
 @NgModule({
--- projects/cardGamesOffline/src/app/modules/user-of-service/user-of-service.module.ts
+++ projects/cardGamesOffline/src/app/modules/user-of-service/user-of-service.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { UserOfServiceStateModule } from "./user-of-service-state/user-of-service-state.module";
 import { UserOfServiceRoutingModule } from "./user-of-service-routing.module";
+import { UserPageComponent } from "./user-page/user-page.component";
+import { MaterialModule } from "../material/material.module";
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, UserOfServiceRoutingModule],
+  declarations: [UserPageComponent],
+  imports: [CommonModule, UserOfServiceRoutingModule, MaterialModule],
 })
 export class UserOfServiceModule {}
