e42ab1fdda1cc4d020fadb216d111961f4605d27
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-06-17 19:00:50 +0200

    add testing cases

--- cypress/integration/login-test.js
+++ cypress/integration/login-test.js
@@ -22,7 +22,24 @@ describe("Test login functionality", () => {
     cy.contains("Hasło jest wymagane!");
     cy.contains("Login jest wymagany!");
   });
-
+  it("Cant login if not typed login", () => {
+    cy.get("mat-icon")
+      .contains(/^person$/)
+      .click();
+    cy.get("[formControlName=passwordOfUser]").type(correctUser.password);
+    cy.get("button").contains("Zaloguj").click();
+    cy.contains("Hasło jest wymagane!").should("not.exist");
+    cy.contains("Login jest wymagany!");
+  });
+  it("Cant login if not typed password", () => {
+    cy.get("mat-icon")
+      .contains(/^person$/)
+      .click();
+    cy.get("[formControlName=loginOfUser]").type(correctUser.username);
+    cy.get("button").contains("Zaloguj").click();
+    cy.contains("Hasło jest wymagane!");
+    cy.contains("Login jest wymagany!").should("not.exist");
+  });
   it("Can login if username and password correct", () => {
     cy.get("mat-icon")
       .contains(/^person$/)
