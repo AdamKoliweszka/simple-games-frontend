9d3848f97a4da4c2a77fae980e797dc3c81c4132
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-06-26 13:06:10 +0200

    add underline top bar to messages page

--- projects/cardGamesOffline/src/app/modules/messages/messages-start-page/messages-start-page.component.ts
+++ projects/cardGamesOffline/src/app/modules/messages/messages-start-page/messages-start-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-messages-start-page',
-  templateUrl: './messages-start-page.component.html',
-  styleUrls: ['./messages-start-page.component.scss']
+  selector: "app-messages-start-page",
+  templateUrl: "./messages-start-page.component.html",
+  styleUrls: ["./messages-start-page.component.scss"],
 })
 export class MessagesStartPageComponent implements OnInit {
+  items = [
+    "MESSAGES_TOP_BAR.CHAT",
+    "MESSAGES_TOP_BAR.FORUM",
+    "MESSAGES_TOP_BAR.PW",
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  get activeIndex() {
+    let url = this.router.url;
+    if (url.indexOf("messages/chat") >= 0) return 0;
+    if (url.indexOf("messages/forum") >= 0) return 1;
+    if (url.indexOf("messages/pw") >= 0) return 2;
   }
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
 }
--- projects/cardGamesOffline/src/app/modules/messages/messages.module.ts
+++ projects/cardGamesOffline/src/app/modules/messages/messages.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MessagesRoutingModule } from "./messages-routing.module";
 import { MessagesStartPageComponent } from "./messages-start-page/messages-start-page.component";
-import { MessagesTopBarComponent } from "./messages-top-bar/messages-top-bar.component";
+
 import { TranslateModule } from "@ngx-translate/core";
+import { UnderlineTopBarModule } from "../design-components/underline-top-bar/underline-top-bar.module";
 
 @NgModule({
-  declarations: [MessagesStartPageComponent, MessagesTopBarComponent],
-  imports: [CommonModule, MessagesRoutingModule, TranslateModule],
+  declarations: [MessagesStartPageComponent],
+  imports: [
+    CommonModule,
+    MessagesRoutingModule,
+    TranslateModule,
+    UnderlineTopBarModule,
+  ],
   bootstrap: [MessagesStartPageComponent],
 })
 export class MessagesModule {}
