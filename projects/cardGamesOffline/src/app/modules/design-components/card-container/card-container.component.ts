8b135bdbcba63f01056b07de5db2fd7860778029
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-07-09 01:51:15 +0200

    add invite friend to users page

--- projects/cardGamesOffline/src/app/modules/design-components/card-container/card-container.component.ts
+++ projects/cardGamesOffline/src/app/modules/design-components/card-container/card-container.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
 export class CardContainerComponent implements OnInit {
   @Input() names: string[];
   @Input() colorsOfMainIcons: string[];
+  @Input() leftIcon: string;
   @Output() leftButtonClick = new EventEmitter<number>();
   @Output() rightButtonClick = new EventEmitter<number>();
   @Output() cardClick = new EventEmitter<number>();
